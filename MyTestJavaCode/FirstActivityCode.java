9eb4eb8ab2eafb92d9be81a9d8ce92d91fff1d26
JaiDuB75 <50184108+JaiDuB75@users.noreply.github.com>
2022-06-14 23:26:52 -0400

    Modified ReadMe and add a directory MyTextJavaCode and created a java file FirstActivityCode in it

---
+++ MyTestJavaCode/FirstActivityCode.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public static class FirstActivityCode
+{
+
+	public static void main(String[] args)
+	{
+		//Assuming that the user inputs a number they are given a random number
+		
+		
+		//First convert the string into an integer
+		
+		int inputNum = Integer.parseInt(args[]); 
+
+		try
+		//Next generate a random number
+
+		Math.random();
+		}
+		catch(NumberFormatExeception e){
+		for(int i = 0; i <= args[0] ; i++){
+		
+		}
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MyTestRepository
+Hi, my name is Jairus DuBose and I am an assocciate at Revature learning to code as a Java Full Stack Developer.
+This is a repository so I can practice concepts taught to me from Revature during my training. 
+
